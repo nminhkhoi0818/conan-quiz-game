2de9633644c5e2c1975791e874680c948bbd39d9
nminhkhoi0818 <nminhkhoi0818@gmail.com>
2022-05-16 20:23:11 +0700

    first commit

---
+++ question.js
@@ -0,0 +1,53 @@
+//Array of Objects
+const quiz=[
+    {
+        q:'Conan nặng bao nhiêu kg ?',
+        options:['17','18','19','20'],
+        answer:1
+    },
+    {
+        q:'Bạn gái của trung sĩ Takagi là ai ?',
+        options:['Haibara','Sato','Ran','Yumi'],
+        answer:1
+    }, 
+    {
+        q:'Biệt danh "Thám tử ngủ gật" chỉ ai ?',
+        options:['Agasa','Kogoro Mori', 'Heiji Hattori', 'Wataru Takagi'],
+        answer:1
+    }, 
+    {
+        q:'Bí danh của Haibara Ai khi còn làm việc cho Tổ chức Áo đen là ?',
+        options:['Vermouth','Sherry','Shery', 'Vodka'],
+        answer:1
+    }, 
+    {
+        q:'Shinichi đã bị teo nhỏ do uống thuốc ?',
+        options:['APTX 1234','APTX 4869','APTX 4986','APTX 2314'],
+        answer:1
+    },
+    {
+        q:'Conan thích môn thể thao nào nhất ?',
+        options:['Bóng rổ','Bóng đá','Cầu lông','Bóng chuyền'],
+        answer:1
+    },
+    {
+        q:'Trong đội thám tử nhỉ có mấy thành viên ?',
+        options:['3','4','5','6'],
+        answer:2
+    },
+    {
+        q:'Sinh nhật của Conan ngày mấy ?',
+        options:['Ngày 3 tháng 5','Ngày 4 tháng 5','Ngày 5 tháng 5','Ngày 6 tháng 5'],
+        answer:1
+    },
+    {
+        q:'21/6 là sinh nhật của ai ?',
+        options:['Mori Ran','Kudo Shinichi','Kaito Kid','Mori Kogoro'],
+        answer:2
+    },
+    {
+        q:'Bạn gái Shinichi là.... ?',
+        options:['Suzuki Sonoko','Mori Ran','Haibara Ai','Nakamori Aoko'],
+        answer:1
+    },
+]
